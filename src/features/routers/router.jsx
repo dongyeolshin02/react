880a4d8798e19b49256a45f3d2b48d1029b78076
dongyeolshin02 <eveing2@gmail.com>
2025-10-31 17:52:09 +0900

    개발 천천히

--- src/features/routers/router.jsx
+++ src/features/routers/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Link, Navigate } from "react-router";
 import Layout from "../../pages/Layout";
-import BestBooksList from "../../pages/BestBooksList";
+import BestBooksList from "../../pages/books/BestBooksList";
+import BookDetail from "../../pages/books/BookDetail";
 
 
 export const router = createBrowserRouter([
@@ -15,8 +16,18 @@ export const router = createBrowserRouter([
 
             },
             {
-                path:'books/best',
-                element: <BestBooksList/>
+                path:'books',
+                children : [
+                    {
+                        path :'best',
+                        element: <BestBooksList/>
+                    },
+                     {
+                        path :'detail',
+                        element: <BookDetail/>
+                    }
+                ]
+              
             }
         ]
     }
---
+++ src/pages/books/BookDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from '../../assets/css/books/book-detail.module.css'
+const BookDetail = () => {
+    return (
+        <div className={style.books}>
+            
+        </div>
+    );
+};
+
+export default BookDetail;
