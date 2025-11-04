cc02f7b27cca8803f22dc1aacbd81d168df58b6f
dongyeolshin02 <eveing2@gmail.com>
2025-11-04 14:30:58 +0900

    aaaa

--- src/pages/BestBooksList.jsx
+++ src/pages/BestBooksList.jsx
@@ -4,6 +4,7 @@ const BestBooksList = () => {
     return (
         <div className='mt-3'>
             <header>
+                
                 <h3>베스트 도서</h3>
             </header>
         </div>
